5d18c658f9c2a33a174cd0b9c8d9f86ad65fc739
mapumarshmallow <mapu.park@gmail.com>
2020-11-18 15:26:12 -0600

    added VoiceToLanguageVoice

--- cloud_functions/VoiceToLanguageChoice.py
+++ cloud_functions/VoiceToLanguageChoice.py
@@ -12,28 +12,28 @@ def detect_language_from_voice(voice_string):
     audio = speech.RecognitionAudio(content=speech_file)
     
     first_lang = 'en-US'
-    second_lang = 'ko'
 
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=48000,
         audio_channel_count=2,
-        language_code=first_lang,
-        alternative_language_codes=[second_lang])
+        language_code=first_lang)
+        # alternative_language_codes=[second_lang])
 
     print('Waiting for operation to complete...')
     response = client.recognize(config=config, audio=audio)
 
     for i, result in enumerate(response.results):
         alternative = result.alternatives[0]
-        print('-' * 20)
-        print(u'First alternative of result {}: {}'.format(i, alternative))
-        print(u'Transcript: {}'.format(alternative.transcript))
-        print(u'Languege Code: {}'.format(result.language_code))
+        language_choice = alternative.transcript
+        # print(u'Transcript: {}'.format(alternative.transcript))
+    
+    return language_choice 
 
 
-speech_file = "test_audio.wav"
-voice_string = encode_voice("test_audio.wav")
-detect_language_from_voice(voice_string)\
+speech_file = "japanese.wav"
+voice_string = encode_voice(speech_file)
+language_choice = detect_language_from_voice(voice_string)
+print(language_choice)
 
 # TODO: talk about hertz and first choice of language. 
